4f3d24293ab356c9cdf89a7e777ce52f89822a58
raulva5 <raulva18@gmail.com>
2015-04-30 19:21:26 +0200

    Update Tasca.hh

--- Tasca/Tasca.hh
+++ Tasca/Tasca.hh
@@ -1,2 +1,41 @@
-#include "tasca.hh"
-string titol;
+/** @file Tasca.hh
+    @brief Classe Tasca
+ */
+
+#ifndef TASCA_HH
+#define TASCA_HH
+#include "../utils.cc"
+#include "../TASCA/Tasca.hh"
+
+/** @class Tasca
+    @brief Representa el títol de la tasca , la/es etiqueta/es
+ 
+ */
+class Tasca {
+
+public:
+	string titol;
+    map<string,bool> metq; // mapa que ens representa les etiquetes ordenades en cas de que hi hagi més d'una
+    Dat d
+private:
+ // Constructores
+    
+    Tasca();
+     
+    // Modificadores
+     /** @brief 
+    \pre 
+    \post */
+    
+    
+    
+    // Consultores
+    /** @brief 
+    \pre: 
+    \post: */
+    
+
+    
+};
+
+#endif
