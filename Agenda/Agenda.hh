6f2f7dd4f82eef095fbd770e5160437e05157449
raulva5 <raulva18@gmail.com>
2015-04-30 19:17:03 +0200

    Update Agenda.hh

--- Agenda/Agenda.hh
+++ Agenda/Agenda.hh
@@ -5,6 +5,7 @@
 #ifndef AGENDA_HH
 #define AGENDA_HH
 #include "../utils.cc"
+#include "Dat.hh"
 
 /** @class Agenda
     @brief Representa una agenda.
@@ -12,29 +13,31 @@
 class Agenda {
 private:
 
+	Dat dt; // rellotge de tasca
+	Dat dt_int; // rellotge intern
+	map<Dat,Tasca> m;
+	vector<Tasca> m;
+
 public:
 	/* Està posat a tot arreu "int i" però faltarà determinar els paràmetres finals*/
 	
   //Constructors
 	Agenda();
-
+	Dat iniciar_rellotge();
   //Modificadors
     //Tasques
-    void apuntar_tasca(int i);
-	void modificar_tasca(int i);
-	void esborrar_tasca(int i);
-  	
-
-  	//Rellotge
-	void canviar_hora();
-	void canviar_data();
-	bool data_posterior();
-
+        void apuntar_tasca(int i);
+	void tractar_modificacio(int i);
+	void tractar_modificacio(const Comanda& c);
+	void modificar_rellotge();
+  	void es_esborrat(int& i,string& etiq);
 
   //Consultors
-	void consultar_tasca(int i);
-
+	void tractar_consulta(const Comanda& c);
+	void tractar_rellotge(const Comanda& c); // en cas de que modifica el rellotge intern crida a modificar_rellotge()
+	void passat();
 
 };
 #endif
+
 
